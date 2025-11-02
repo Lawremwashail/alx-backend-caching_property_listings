8b90313dc291c7ec54c131464c522afbcaa0e45f
Lawremwashail <lawremwash@gmail.com>
2025-11-02 20:56:37 +0300

    Caching in Django

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)  # cache for 15 minutes
+def property_list(request):
+    """Return a list of all properties (cached)."""
+    properties = Property.objects.all().values(
+        "id", "title", "description", "price", "location", "created_at"
+    )
+    return JsonResponse(list(properties), safe=False)
+
